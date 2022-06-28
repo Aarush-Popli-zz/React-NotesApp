21e34464fcbc0a69ba636e547be52e40c5c7f706
Aarush Popli <popliaarush00@gmail.com>
2022-06-28 15:51:49 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,7 @@ const port = 5000
 app.use(express.json())
 app.use(cors())
 
-app.use(cors())
-
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-app.listen(port, () => {
-  console.log(`Notes App listening on port ${port}`)
-})
+app.listen(process.env.PORT || port)

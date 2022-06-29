61f4d3cdd365df546ced7ff7a018a7f84b682c5c
Aarush Popli <popliaarush00@gmail.com>
2022-06-29 14:05:46 +0530

    deploy

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/notesapp";
+const mongoURI = "mongodb+srv://Aarush_Notes:!%40%23%24AarushPopli00@cluster0.sokqf5l.mongodb.net/?retryWrites=true&w=majority";
 const connectToMongo = async ()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log("Connected to mongo successfully");
